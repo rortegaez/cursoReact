c6e874265d42673c61f9bbc2635a0b5b670939c0
rortegaez <raulor83@gmail.com>
2024-11-01 07:58:26 +0100

    begining tic tac toe

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,6 +1,47 @@
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
+const users = [
+  {
+    userName: "midudev",
+    name: "Miguel Ángel Durán",
+    ifFollowing: true,
+  },
+  {
+    userName: "pheralb",
+    name: "Pablo Heraldo",
+    ifFollowing: false,
+  },
+  {
+    userName: "PacoHdezs",
+    name: "Paco Hdez",
+    ifFollowing: true,
+  },
+  {
+    userName: "TMChein",
+    name: "Tomás",
+    ifFollowing: false,
+  },
+];
+
 export function App() {
-  return <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán" />;
+  /*
+  Esta son dos formas de pasar props
+  const midudev = { userName: "midudev" }; // otra mandera de mandar las props
+
+  return (
+    <>
+      <TwitterFollowCard {...midudev}>Miguel Angel Duran</TwitterFollowCard>;
+      <TwitterFollowCard userName="pheralb">Pablo Hernandez</TwitterFollowCard>
+    </>
+  ); */
+  return (
+    <section className="App">
+      {users.map(({ userName, name, ifFollowing }) => {
+        <TwitterFollowCard userName={userName} initialFollowing={ifFollowing}>
+          {name}
+        </TwitterFollowCard>;
+      })}
+    </section>
+  );
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,4 +1,18 @@
-export function TwitterFollowCard({ userName, name, ifFollowing }) {
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+export function TwitterFollowCard({ children, userName, initialIfFollowing }) {
+  const [ifFollowing, setIfFollowing] = useState(initialIfFollowing);
+
+  const text = ifFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = ifFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
+  const handleClick = () => {
+    setIfFollowing(!ifFollowing);
+  };
+
   return (
     <article className="tw-followCard">
       <header className="tv-followCard-header">
@@ -8,14 +22,24 @@ export function TwitterFollowCard({ userName, name, ifFollowing }) {
           src={`https://unavatar.io/${userName}`}
         />
         <div className="tw-followCard-info">
-          <strong>{name}</strong>
+          <strong>{children}</strong>
           <span className="tw-followCard-infoUserName">@{userName}</span>
         </div>
 
         <aside>
-          <button className="tw-followCard-button">Seguir</button>
+          <button className={buttonClassName} onClick={handleClick}>
+            <span className="tw-followCard-text">{text}</span>
+            <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+          </button>
         </aside>
       </header>
     </article>
   );
 }
+TwitterFollowCard.propTypes = {
+  userName: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  ifFollowing: PropTypes.bool,
+  children: PropTypes.string.isRequired,
+  initialIfFollowing: PropTypes.bool,
+};
