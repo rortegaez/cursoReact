d3eba4626060c92933b90776033d49a3897c8ea9
rortegaez <raulor83@gmail.com>
2024-11-16 10:51:52 +0100

    finish tictactoe

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -7,8 +7,16 @@ import { TURNS } from "./constants.jsx";
 import { checkEndGame, checkWinner } from "./logic/board.js";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromLocalStorage = window.localStorage.getItem("board");
+    return boardFromLocalStorage
+      ? JSON.parse(boardFromLocalStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
   // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null);
 
@@ -16,6 +24,9 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("newTurn");
   };
 
   const updateBoard = (index) => {
@@ -28,6 +39,9 @@ function App() {
     // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    // guardar aqui partidad
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
     // revisar si hay ganador
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
@@ -42,6 +56,7 @@ function App() {
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
+
       <section className="game">
         {board.map((_, index) => {
           return (
