6a1a84ed62339e4b5fa58e97e9c0fb15f0191c9c
rortegaez <raulor83@gmail.com>
2024-11-17 09:28:04 +0100

    finish third project

---
+++ projects/03-mouse-follower/src/components/FollowMouse.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("efecto", { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log("handleMove", { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    // limpiar los useEffect
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  return (
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <h1>Proyecto 3</h1>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} Seguir puntero
+      </button>
+    </main>
+  );
+};
